e2944899596a131f86654e3926c44a450b32e1c1
itscopebd <itscopebd@gmail.com>
2023-04-12 17:46:27 +0600

    responsive home page and navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 const Header = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
         <div className='bg py-5'>
-            <div className='my-container hidden md:flex justify-between items-center'>
+            <div className='my-container flex justify-between items-center relative'>
                 <div className="logo__text">
                     <Link to="/">
                         <h4 className='text-2xl italic'> <span className='text-[#7E90FE]'>Job</span><span className='text-[#9873FF]'>Hunter</span></h4>
                     </Link>
                 </div>
-                <nav>
+                <nav className='hidden md:flex'>
                     <ul className='flex gap-10 items-center'>
                         <li> <NavLink to="/" className={({ isActive }) => (isActive ? "text-blue-500" : "")}>Home</NavLink> </li>
                         <li> <NavLink to="/statistics" className={({ isActive }) => (isActive ? "text-blue-500" : "")}>Statistics</NavLink> </li>
@@ -19,34 +20,43 @@ const Header = () => {
 
                     </ul>
                 </nav>
-                <div className="btn-wrapp">
+                <div className="btn-wrapp hidden md:flex">
                     <button className='btn'>Star Applying</button>
                 </div>
-            </div>
 
-            {/* mobile header  */}
-            <div className='my-container flex md:hidden justify-between items-center'>
-                <div className="logo__text">
-                    <Link to="/">
-                        <h4 className='text-2xl italic'> <span className='text-[#7E90FE]'>Job</span><span className='text-[#9873FF]'>Hunter</span></h4>
-                    </Link>
-                </div>
-                <nav>
-                    <ul className='flex gap-10 items-center'>
-                        <li> <NavLink to="/">Home</NavLink> </li>
-                        <li> <NavLink to="/statistics">Statistics</NavLink> </li>
-                        <li> <NavLink to="/applyedJobs">Applied Jobs</NavLink> </li>
-                        <li> <NavLink to="/blog">Blog</NavLink> </li>
+                {/* mobile menu  */}
+                <nav className='md:hidden'>
+                    <button onClick={() => setIsMenuOpen(true)}>
+                        <Bars3Icon className="h-6 w-6 text-black" />
+                    </button>
+                    {
+                        isMenuOpen && (
+                            <div className=' absolute left-3 top-3  w-96 bg-white p-4 rounded-lg duration-1000'>
+                                <button onClick={() => setIsMenuOpen(false)}>
+                                    <XMarkIcon className="h-6 w-6 text-blue-700 absolute top-3 right-3" />
+                                </button>
+                                <ul className='flex flex-col gap-3 items-center absolute left-2 top-2 py-5 bg-black text-white rounded-lg w-5/6 '>
+                                    <li className='text-left w-12/12'> <NavLink to="/">Home</NavLink> </li>
+                                    <li> <NavLink to="/statistics">Statistics</NavLink> </li>
+                                    <li> <NavLink to="/applyedJobs">Applied Jobs</NavLink> </li>
+                                    <li> <NavLink to="/blog">Blog</NavLink> </li>
+                                    <li><button className='btn'>Star Applying</button></li>
 
-                    </ul>
+                                </ul>
+                            </div>
+                        )
+                    }
                 </nav>
-                <div className="btn-wrapp">
-                    <button className='btn'>Star Applying</button>
-                </div>
+
             </div>
 
 
         </div>
+
+
+
+
+
     );
 };
 
